626a257df6c70a375128a891c54cebcf4659b987
Tristan Goel <trgoel@c3r5s4.42lausanne.ch>
2025-09-15 15:14:06 +0200

    from_branch_working

--- bonus/srcs/maths/triangle.c
+++ bonus/srcs/maths/triangle.c
@@ -87,25 +87,22 @@ void intersect_triangle_quad(t_ray *ray, t_face *face, t_inter *inter)
 
 	e[0] = sub_vectors_no_v(face->vertices[1], face->vertices[0]);
 	e[1] = sub_vectors_no_v(face->vertices[2], face->vertices[0]);
-	p = sub_vectors_no_v(&ray->start, face->vertices[0]);
 	cross_product(&ray->dir, &e[1], &d);
 	det = dot_product(&e[0], &d);
-	printf("det: %f\n", det);
 	if (fabs(det) < 1e-6)
 		return;
+	p = sub_vectors_no_v(&ray->start, face->vertices[0]);
 	u = dot_product(&p, &d) / det;
-	printf("u: %f\n", u);
 	if (u < 0.0f || u > 1)
 		return ;
 	cross_product(&p, &e[0], &q);
-	print_vec(q);
 	v = dot_product(&ray->dir, &q) / det;
-	printf("v: %f\n", v);
 	if (v < 0.0f || u + v > 1)
 		return ;
 	t = dot_product(&e[1], &q) / det;
-	printf("t: %f\n", t);
 	if (t < -1e-6)
+		return ;
+	if (t > inter->t)
 		return ;
 	inter->t = t;
 	cross_product(&e[0], &e[1], &inter->normale);
@@ -118,6 +115,7 @@ void intersect_quad(t_ray *ray, t_face *face)
 	t_inter	inter;
 
 	inter.found = 0;
+	inter.t = INFINITY;
 	new.vertices[0] = face->vertices[0];
 	new.vertices[1] = face->vertices[1];
 	new.vertices[2] = face->vertices[2];
